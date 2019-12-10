0b1caf147584a4d3160bab7fb2f17ae196d3e88e
jmknopf1007 <jmknopf1007@gmail.com>
2019-12-10 10:48:48 -0800

    Done.

--- app.rb
+++ app.rb
@@ -4,16 +4,17 @@ class App < Sinatra::Base
   # Write your code here!
 
   get "/reversename/:name" do
-    "#{params[:name].reverse}"
+    params[:name].reverse.to_s
   end
 
   get "/square/:number" do
-    "#{params[:number].to_i ** 2}"
+    (params[:number].to_i ** 2).to_s
   end
 
   get "/say/:number/:phrase" do
     "#{params[:phrase] * params[:number].to_i}"
   end
+
 
   get "/say/:word1/:word2/:word3/:word4/:word5" do 
     "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
